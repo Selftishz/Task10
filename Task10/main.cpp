95b42cba1bcabdf187b94d890e5b6688176c32cf
Artyom <Artyom@DESKTOP-HB4QEA4>
2022-05-06 23:18:10 +0300

    Добавьте файлы проекта.

---
+++ Task10/main.cpp
@@ -0,0 +1,8 @@
+﻿#include "Func.h"
+int main() {
+	setlocale(0, "");
+	Student student("Vasyan", "Vasya", "Vasilevich", 2, 5);
+	Boss boss("Lehin", "Leha", "Lehovich", 1, 23);
+	student.print();
+	boss.print();
+}
